f3ef10b3612733ff9bda2ef3ce2972fa6f0c4a5e
Sayantika Laskar <127471376+SayantikaLaskar@users.noreply.github.com>
2025-06-10 09:16:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,9 +56,9 @@ class WalmartDataset(Dataset):
         return len(self.df)
 
    def __getitem__(self, idx):
-    x = torch.tensor(self.features[idx], dtype=torch.float32)
-    y = torch.tensor(self.targets[idx], dtype=torch.float32)
-    return x, y
+        x = torch.tensor(self.features[idx], dtype=torch.float32)
+        y = torch.tensor(self.targets[idx], dtype=torch.float32)
+        return x, y
 
 # 3. Model
 class DemandModel(nn.Module):
